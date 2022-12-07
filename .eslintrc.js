c7854f037124caa90b5514356dade056d6a14d95
felipeLeao18 <flpleao1807@gmail.com>
2022-12-07 12:09:28 -0300

    chore: add eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  rules: {
+    indent: 'off',
+    allowExpressions: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    'no-console': 'warn',
+    'no-misudes-promises': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/no-misuses-promises': 'off',
+    '@typescript-eslint/no-base-to-string': 'off'
+  }
+}
